97e95b84704ddb46523ad167be87ef39b690af19
sztoslol <kubuszeski05@wp.pl>
2022-10-21 14:57:38 +0200

    pt 2 - add, replace btn

--- app/src/main/java/com/example/zad1013/MainActivity.kt
+++ app/src/main/java/com/example/zad1013/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.zad1013
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tekst = findViewById<EditText>(R.id.editTextTextPersonName)
+
+        var ok = findViewById<TextView>(R.id.label)
+
+
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            ok.text = ok.text.toString() +" "+ tekst.text.toString()
+            tekst.text.clear()
+        }
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            ok.text = "Dominik Schwoch"
+            tekst.text.clear()
+        }
     }
 }
